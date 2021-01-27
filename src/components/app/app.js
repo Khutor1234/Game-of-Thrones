464f6dd6dde0e770cbfc2d9033a41ff365097dcd
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-01-27 14:40:37 +0200

    Code refactoring, patterns

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,11 +4,17 @@ import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../errorMessage';
 import CharacterPage from '../characterPage';
+import ItemList from '../itemList';
+import CharDetails from '../charDetails';
+import gotService from '../../services/gotService';
 
 import './app.css';
 
 
 export default class App extends Component {
+
+    gotService = new gotService();
+
     state = {
         showRandomChar: true,
         error: false
@@ -41,6 +47,28 @@ export default class App extends Component {
                         </Col>
                     </Row>
                     <CharacterPage/>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList 
+                                onItemSelected={this.onItemSelected}
+                                getData = {this.gotService.getAllBooks}
+                                renderItem = {(item) => item.name}/>
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails charId = {this.state.selectedChar} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList 
+                                onItemSelected={this.onItemSelected}
+                                getData = {this.gotService.getAllHouses}
+                                renderItem = {(item) => item.name}/>
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails charId = {this.state.selectedChar} />
+                        </Col>
+                    </Row>
                 </Container>
             </>
         );
